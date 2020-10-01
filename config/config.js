e306474b423c526541f6bf3368b0fccde2098d92
dayvisons <dayvisons@gmail.com>
2020-10-01 20:48:55 -0300

    api-sequelize

---
+++ config/config.js
@@ -0,0 +1,20 @@
+module.exports = {
+{
+    development: {
+        database: {
+            host: 'localhost',
+            port: 3306,
+            name: 'cadastro',
+            dialect: 'mysql',
+            user: 'root',
+            password: ''
+        }
+    }
+    production: {
+        database: {
+            host: process.env.DB_HOST,
+            port: process.env.DB_HOST
+        }
+    }
+}
+}
